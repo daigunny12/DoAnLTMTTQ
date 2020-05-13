5169d9a25db55d1b85160dd45e4112bc0fc5015b
daigunny12 <52237336+daigunny12@users.noreply.github.com>
2020-05-13 14:55:11 +0700

    udate

--- QuanLyCuaHangPhanBon/GUI/Form1.Designer.cs
+++ QuanLyCuaHangPhanBon/GUI/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(868, 483);
+            this.ClientSize = new System.Drawing.Size(839, 483);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
