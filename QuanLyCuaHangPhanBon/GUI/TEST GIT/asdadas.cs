a5703c19da3b3ffb0bce087009a8becf3ffec352
thanhson391999 <55115530+thanhson391999@users.noreply.github.com>
2020-06-01 13:43:38 +0700

    ok ok

---
+++ QuanLyCuaHangPhanBon/GUI/TEST GIT/asdadas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.TEST_GIT
+{
+    public partial class asdadas : Form
+    {
+        public asdadas()
+        {
+            InitializeComponent();
+        }
+    }
+}
